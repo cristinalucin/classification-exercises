bb701385b6cf226d58cc31ff123e0c196f35b414
Cristina Lucin <cristinajlucin@gmail.com>
2022-10-17 19:11:49 -0500

    updated prepare.py file

--- prepare.py
+++ prepare.py
@@ -1,13 +1,25 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+#--------IRIS DATA-----------------------------------------
+
 def prep_iris(df):
     # Drop duplicates
     df.drop_duplicates(inplace=True)
+    #Rename column
+    df = df.rename(columns={'species_name': 'species'})
     # Drop columns 
     columns_to_drop = ['species_id', 'measurement_id']
     df = df.drop(columns = columns_to_drop)
     # encoded categorical variables
-    dummy_df = pd.get_dummies(df[['species_name']], dummy_na=False, drop_first=True)
+    dummy_df = pd.get_dummies(df[['species']], dummy_na=False, drop_first=True)
     df = pd.concat([df, dummy_df], axis=1)
     return df
+
+#--------TITANIC DATA--------------------------------------
 
 def prep_titanic(df):
     # Drop duplicates
@@ -20,6 +32,8 @@ def prep_titanic(df):
     df = pd.concat([df, dummy_df], axis=1)
     return df
 
+#-------TELCO DATA---------------------------------------------
+
 def prep_telco(df):
     # Drop duplicates
     df.drop_duplicates(inplace=True)
@@ -31,3 +45,19 @@ def prep_telco(df):
     df = pd.concat([df, dummy_df], axis=1)
     return df
 
+def split_telco_data(df):
+    train_validate, test = train_test_split(df, test_size=.2, 
+                                        random_state=123, 
+                                        stratify=df.churn)
+    train, validate = train_test_split(train_validate, test_size=.3, 
+                                   random_state=123, 
+                                   stratify=train_validate.churn)
+    return train, validate, test
+
+
+def my_train_test_split(df, target):
+    
+    train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[target])
+    train, validate = train_test_split(train, test_size=.25, random_state=123, stratify=train[target])
+    
+    return train, validate, test
