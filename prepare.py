7c6539b7950395627a18af3294746f35f498da3a
Cristina Lucin <cristinajlucin@gmail.com>
2022-10-17 19:37:49 -0500

    updated classification exercises with prepare exercises

--- prepare.py
+++ prepare.py
@@ -4,12 +4,10 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 
-#--------IRIS DATA-----------------------------------------
-
 def prep_iris(df):
     # Drop duplicates
     df.drop_duplicates(inplace=True)
-    #Rename column
+    #rename column
     df = df.rename(columns={'species_name': 'species'})
     # Drop columns 
     columns_to_drop = ['species_id', 'measurement_id']
@@ -18,8 +16,6 @@ def prep_iris(df):
     dummy_df = pd.get_dummies(df[['species']], dummy_na=False, drop_first=True)
     df = pd.concat([df, dummy_df], axis=1)
     return df
-
-#--------TITANIC DATA--------------------------------------
 
 def prep_titanic(df):
     # Drop duplicates
@@ -32,8 +28,6 @@ def prep_titanic(df):
     df = pd.concat([df, dummy_df], axis=1)
     return df
 
-#-------TELCO DATA---------------------------------------------
-
 def prep_telco(df):
     # Drop duplicates
     df.drop_duplicates(inplace=True)
@@ -44,16 +38,6 @@ def prep_telco(df):
     dummy_df = pd.get_dummies(df[['gender','partner','dependents','phone_service', 'multiple_lines','online_security','online_backup','device_protection','tech_support','streaming_tv','streaming_movies','paperless_billing','churn','contract_type','internet_service_type','payment_type']], dummy_na=False, drop_first=[True, True])
     df = pd.concat([df, dummy_df], axis=1)
     return df
-
-def split_telco_data(df):
-    train_validate, test = train_test_split(df, test_size=.2, 
-                                        random_state=123, 
-                                        stratify=df.churn)
-    train, validate = train_test_split(train_validate, test_size=.3, 
-                                   random_state=123, 
-                                   stratify=train_validate.churn)
-    return train, validate, test
-
 
 def my_train_test_split(df, target):
     
