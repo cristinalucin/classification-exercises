3511192dabb14a4316c4f45d27e74228e228a018
Cristina Lucin <cristinajlucin@gmail.com>
2022-10-23 09:35:27 -0500

    updated prepare file

--- prepare.py
+++ prepare.py
@@ -4,6 +4,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 
+#_______________Iris______________#
+
 def prep_iris(df):
     # Drop duplicates
     df.drop_duplicates(inplace=True)
@@ -13,19 +15,28 @@ def prep_iris(df):
     columns_to_drop = ['species_id', 'measurement_id']
     df = df.drop(columns = columns_to_drop)
     # encoded categorical variables
-    dummy_df = pd.get_dummies(df[['species']], dummy_na=False, drop_first=True)
+    dummy_df = pd.get_dummies(df[['species']], dummy_na=False)
     df = pd.concat([df, dummy_df], axis=1)
     return df
 
 def prep_titanic(df):
-    # Drop duplicates
-    df.drop_duplicates(inplace=True)
-    # Drop columns 
-    columns_to_drop = ['embarked', 'pclass', 'passenger_id', 'deck']
-    df = df.drop(columns = columns_to_drop)
-    # encoded categorical variables
-    dummy_df = pd.get_dummies(df[['sex', 'class', 'embark_town']], dummy_na=False, drop_first=[True, True])
-    df = pd.concat([df, dummy_df], axis=1)
+    '''
+    take in titanc dataframe, remove all rows where age or embarked is null, 
+    get dummy variables for sex, embark_town, and pclass
+    and drop sex, deck, passenger_id, class, and embark_town. 
+    '''
+
+    df = df[(df.age.notna()) & (df.embarked.notna())]
+    df = df.drop(columns=['deck', 'passenger_id', 'class'])
+
+    dummy_df = pd.get_dummies(df[['sex', 'embark_town']], prefix=['sex', 'embark'])
+
+    df = pd.concat([df, dummy_df.drop(columns=['sex_male'])], axis=1)
+
+    df = df.drop(columns=['sex', 'embark_town']) 
+
+    df = df.rename(columns={"sex_female": "is_female"})
+
     return df
 
 def prep_telco(df):
@@ -39,9 +50,28 @@ def prep_telco(df):
     df = pd.concat([df, dummy_df], axis=1)
     return df
 
+#_____________Train Test Split_______________#
+
 def my_train_test_split(df, target):
     
     train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[target])
     train, validate = train_test_split(train, test_size=.25, random_state=123, stratify=train[target])
     
+    return train, validate, test
+
+def train_validate_test_split(df, target, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    train_validate, test = train_test_split(df, test_size=0.1, 
+                                            random_state=seed, 
+                                            stratify=df[target])
+    train, validate = train_test_split(train_validate, test_size=0.11111, 
+                                       random_state=seed,
+                                       stratify=train_validate[target])
     return train, validate, test
