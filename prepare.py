4ff01857d0b616385fdae5c6e4d87d3e4d52c2df
Cristina Lucin <cristinajlucin@gmail.com>
2022-10-23 12:58:56 -0500

    updated prepare file

--- prepare.py
+++ prepare.py
@@ -75,14 +75,40 @@ def impute_mean_age(train, validate, test):
 #-------------------TELCO-----------------#
 
 def prep_telco(df):
-    # Drop duplicates
-    df.drop_duplicates(inplace=True)
-    # Drop columns 
-    columns_to_drop = ['payment_type_id', 'internet_service_type_id', 'contract_type_id']
-    df = df.drop(columns = columns_to_drop)
-    # encoded categorical variables
-    dummy_df = pd.get_dummies(df[['gender','partner','dependents','phone_service', 'multiple_lines','online_security','online_backup','device_protection','tech_support','streaming_tv','streaming_movies','paperless_billing','churn','contract_type','internet_service_type','payment_type']], dummy_na=False, drop_first=[True, True])
+    # Drop duplicate columns
+    df.drop(columns=['payment_type_id', 'internet_service_type_id', 'contract_type_id', 'customer_id'], inplace=True)
+       
+    # Drop null values stored as whitespace    
+    df['total_charges'] = df['total_charges'].str.strip()
+    df = df[df.total_charges != '']
+    
+    # Convert to correct datatype
+    df['total_charges'] = df.total_charges.astype(float)
+    
+    # Convert binary categorical variables to numeric
+    df['gender_encoded'] = df.gender.map({'Female': 1, 'Male': 0})
+    df['partner_encoded'] = df.partner.map({'Yes': 1, 'No': 0})
+    df['dependents_encoded'] = df.dependents.map({'Yes': 1, 'No': 0})
+    df['phone_service_encoded'] = df.phone_service.map({'Yes': 1, 'No': 0})
+    df['paperless_billing_encoded'] = df.paperless_billing.map({'Yes': 1, 'No': 0})
+    df['churn_encoded'] = df.churn.map({'Yes': 1, 'No': 0})
+    
+    # Get dummies for non-binary categorical variables
+    dummy_df = pd.get_dummies(df[['multiple_lines', \
+                              'online_security', \
+                              'online_backup', \
+                              'device_protection', \
+                              'tech_support', \
+                              'streaming_tv', \
+                              'streaming_movies', \
+                              'contract_type', \
+                              'internet_service_type', \
+                              'payment_type']], dummy_na=False, \
+                              drop_first=True)
+    
+    # Concatenate dummy dataframe to original 
     df = pd.concat([df, dummy_df], axis=1)
+    
     return df
 
 #_____________Train Test Split_______________#
