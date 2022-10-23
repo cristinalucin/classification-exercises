2f22673ed87f70a8cfca9e95cdee13b759fe42c3
Cristina Lucin <cristinajlucin@gmail.com>
2022-10-23 10:28:53 -0500

    updated prepare file with titanic imputers

--- prepare.py
+++ prepare.py
@@ -19,25 +19,60 @@ def prep_iris(df):
     df = pd.concat([df, dummy_df], axis=1)
     return df
 
+#------------------------TITANIC----------------------#
+
 def prep_titanic(df):
     '''
-    take in titanc dataframe, remove all rows where age or embarked is null, 
+    take in titanc dataframe, remove all rows where age is null, 
     get dummy variables for sex, embark_town, and pclass
-    and drop sex, deck, passenger_id, class, and embark_town. 
+    and drop sex, deck, passenger_id, class, and embarked. 
     '''
 
-    df = df[(df.age.notna()) & (df.embarked.notna())]
-    df = df.drop(columns=['deck', 'passenger_id', 'class'])
+    df = df[(df.age.notna())]
+    df = df.drop(columns=['deck', 'passenger_id', 'class', 'embarked'])
 
     dummy_df = pd.get_dummies(df[['sex', 'embark_town']], prefix=['sex', 'embark'])
 
     df = pd.concat([df, dummy_df.drop(columns=['sex_male'])], axis=1)
 
-    df = df.drop(columns=['sex', 'embark_town']) 
+    df = df.drop(columns=['sex']) 
 
     df = df.rename(columns={"sex_female": "is_female"})
 
     return df
+
+def impute_titanic_mode(train, validate, test):
+    '''
+    Takes in train, validate, and test, and uses train to identify the best value to replace nulls in embark_town
+    Imputes that value into all three sets and returns all three sets
+    '''
+    imputer = SimpleImputer(missing_values = np.nan, strategy='most_frequent')
+    train[['embark_town']] = imputer.fit_transform(train[['embark_town']])
+    validate[['embark_town']] = imputer.transform(validate[['embark_town']])
+    test[['embark_town']] = imputer.transform(test[['embark_town']])
+    return train, validate, test
+
+
+def impute_mean_age(train, validate, test):
+    '''
+    This function imputes the mean of the age column for
+    observations with missing values.
+    Returns transformed train, validate, and test df.
+    '''
+    # create the imputer object with mean strategy
+    imputer = SimpleImputer(strategy = 'mean')
+    
+    # fit on and transform age column in train
+    train['age'] = imputer.fit_transform(train[['age']])
+    
+    # transform age column in validate
+    validate['age'] = imputer.transform(validate[['age']])
+    
+    # transform age column in test
+    test['age'] = imputer.transform(test[['age']])
+    
+    return train, validate, test
+#-------------------TELCO-----------------#
 
 def prep_telco(df):
     # Drop duplicates
@@ -54,8 +89,8 @@ def prep_telco(df):
 
 def my_train_test_split(df, target):
     
-    train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[target])
-    train, validate = train_test_split(train, test_size=.25, random_state=123, stratify=train[target])
+    train, test = train_test_split(df, test_size=.15, random_state=123, stratify=df[target])
+    train, validate = train_test_split(train, test_size=.1765, random_state=123, stratify=train[target])
     
     return train, validate, test
 
@@ -64,14 +99,14 @@ def train_validate_test_split(df, target, seed=123):
     This function takes in a dataframe, the name of the target variable
     (for stratification purposes), and an integer for a setting a seed
     and splits the data into train, validate and test. 
-    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
-    original dataset, and train is .70*.80= 56% of the original dataset. 
+    Test is 15% of the original dataset, validate is .1765*.85= 15% of the 
+    original dataset, and train is 75% of the original dataset. 
     The function returns, in this order, train, validate and test dataframes. 
     '''
-    train_validate, test = train_test_split(df, test_size=0.1, 
+    train_validate, test = train_test_split(df, test_size=0.15, 
                                             random_state=seed, 
                                             stratify=df[target])
-    train, validate = train_test_split(train_validate, test_size=0.11111, 
+    train, validate = train_test_split(train_validate, test_size=0.1765, 
                                        random_state=seed,
                                        stratify=train_validate[target])
     return train, validate, test
